a0d145fe5017f5a3de34011cc0e06d9bb4d0b47b
lostanush <lostanush01@gmail.com>
2024-06-28 21:33:31 +0530

    Start project

---
+++ merncamp/client/pages/login.js
@@ -0,0 +1,12 @@
+const login = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Login Page : </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default login;
---
+++ merncamp/client/pages/register.js
@@ -0,0 +1,12 @@
+const register = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Register Page : </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default register;
