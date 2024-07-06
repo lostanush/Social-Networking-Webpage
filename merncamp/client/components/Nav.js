e96b80825ac54ca9dedaef2b84d05f4fd158d764
lostanush <lostanush01@gmail.com>
2024-07-06 09:59:43 +0530

    Added Logout button

--- merncamp/client/components/Nav.js
+++ merncamp/client/components/Nav.js
@@ -1,6 +1,18 @@
+import { useContext } from "react";
+import { UserContext } from "../context";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Nav = () => {
+  const [state, setState] = useContext(UserContext);
+  const router = useRouter();
+
+  const logout = () => {
+    window.localStorage.removeItem("auth");
+    setState(null);
+    router.push("/login");
+  };
+
   return (
     <nav className="nav d-flex justify-content-begin a">
       <Link className="nav-link logo text-light" href="/">
@@ -14,6 +26,10 @@ const Nav = () => {
       <Link className="nav-link text-light" href="/register">
         Register
       </Link>
+
+      <a onClick={logout} className="nav-link text-light" href="/login">
+        Logout
+      </a>
     </nav>
   );
 };
