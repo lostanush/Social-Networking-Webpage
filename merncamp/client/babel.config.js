531c03940fbefec5a7bedc3d7066f49309d9bbe7
Anush Dighore <lostanush01@gmail.com>
2024-07-11 16:46:57 +0530

    Delete merncamp directory

--- merncamp/client/babel.config.js
+++
@@ -1,9 +0,0 @@
-// babel.config.js
-module.exports = {
-  presets: [
-    "next/babel", // Use the Next.js preset
-  ],
-  plugins: [
-    // Add any additional plugins here
-  ],
-};
--- merncamp/server/middleware/auth.js
+++
@@ -1,7 +0,0 @@
-import { expressjwt } from "express-jwt";
-
-export const requireSignin = expressjwt({
-  secret: process.env.JWT_SECRET,
-  algorithms: ["HS256"],
-  userProperty: 'auth', // This sets the property name in req object where decoded token is attached
-});
