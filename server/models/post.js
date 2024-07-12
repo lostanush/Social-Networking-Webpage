ca0c300cbf1c265b9f48bd5c7caa6f582b9321ff
lostanush <lostanush01@gmail.com>
2024-07-12 19:21:47 +0530

    Created post model

---
+++ server/models/post.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const { ObjectId } = mongoose.Schema;
+
+const postSchema = new mongoose.Schema(
+  {
+    content: {
+      type: {},
+      required: true,
+    },
+    postBy: {
+      type: String,
+      ref: "User",
+    },
+    image: {
+      url: String,
+      public_id: String,
+    },
+    likes: [{ type: ObjectId, ref: "User" }],
+    comments: [
+      {
+        text: String,
+        created: { type: Date, default: Date.now },
+        postedBy: { type: ObjectId, ref: "User" },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.modal("Post", postSchema);
