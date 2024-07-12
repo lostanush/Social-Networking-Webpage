87b6d7f720f81c60e28edf008a41fd0dc9cc834e
lostanush <lostanush01@gmail.com>
2024-07-12 19:21:05 +0530

    Created post request

--- client/pages/users/dashboard.js
+++ client/pages/users/dashboard.js
@@ -1,19 +1,44 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../../context";
 import UserRoute from "../../components/router/UserRoute";
+import CreatePostForm from "../../components/CreatePostForm";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 const dashboard = () => {
   const [state] = useContext(UserContext);
+  const [content, setContent] = useState("");
+  const router = useRouter();
+
+  const postSubmit = (e) => {
+    e.preventDefault();
+    // console.log("post", content);
+    try {
+      const { data } = axios.post("/create-post", { content });
+      console.log("create post responce : ", data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <UserRoute>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1 className="display-1 text-center py-5">Dashboard : </h1>
-            {JSON.stringify(state)}
-            <img src="/images/yourname.png" alt="image" />
+      <div className="container-fluid">
+        <div className="row py-5 bg-default-img text-light">
+          <div className="col text-center">
+            <h1>Newsfeed</h1>
           </div>
+        </div>
+
+        <div className="row py-3">
+          <div className="col-md-8">
+            <CreatePostForm
+              content={content}
+              setContent={setContent}
+              postSubmit={postSubmit}
+            />
+          </div>
+          <div className="col-md-4">Sidebar</div>
         </div>
       </div>
     </UserRoute>
